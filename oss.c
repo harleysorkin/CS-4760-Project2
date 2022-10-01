9498467ced1d14c0cae8245a4fda71eb75596d24
harleysorkin <hsrzg@umsl.edu>
2022-09-30 20:16:51 -0500

    Added comments

--- oss.c
+++ oss.c
@@ -19,18 +19,22 @@ void alarmInterrupt(int sig);
 
 int main(int argc, char* argv[]) {
     
+    // Default values
     int opt;
     int n = 4;
     int s = 2;
     int m = 1;
 
+    // Using getopt to parse arguments
     while((opt = getopt(argc, argv, ":hn:s:m:")) != -1) {
         switch(opt) {
 
+	    // Help message
             case 'h':
                 printf("Invalid input. Expected arguments:\n./oss -n x -s y -m z\nWhere \'n\' is the max number of processes to fork (max 18, default 4)\n\'s\' is the number of children that can exist at the same time (default 2)\nand \'m\' is the amount to increase the clock multiplied by 1 billion (default 1)\n");
                 return 0;
 
+	    // Set value of n if n !> 18
             case 'n':
 		n = atoi(optarg);
                 if(n > 18) {
@@ -39,14 +43,17 @@ int main(int argc, char* argv[]) {
                 }
 		break;
 
+	    // Set value of s
             case 's':
                 s = atoi(optarg);
 		break;
 
+	    // Set value of m
             case 'm':
                 m = atoi(optarg);
 		break;
 
+	    // Unknown argument passed
             case '?':
                 printf("Invalid input. Expected arguments:\n./oss -n x -s y -m z\nWhere \'n\' is the max number of processes to fork (max 18, default 4)\n\'s\' is the number of children that can exist at the same time (default 2)\nand \'m\' is the amount to increase the clock multiplied by 1 billion (default 1)\n");
                 return 0;
@@ -54,11 +61,14 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Creates alarm set for 2 seconds. Terminates program if it runs any longer
     signal(SIGALRM, alarmTime);
     alarm(2);
 
+    // Allows for 'ctrl + c' termination
     signal(SIGINT, alarmInterrupt);
 
+    // Create shared memory using shmget
     shmid = shmget(SHMKEY, BUFF_SIZE, 0777 | IPC_CREAT);
 
     if(shmid == -1) {
@@ -66,6 +76,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    // Pointer to shared memory
     char *shmem = shmat(shmid, 0, 0);
 
     if(shmem == (char *) -1) {
@@ -73,6 +84,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    // Creating clock in shared memory
     int *clockSec = (int *) shmem;
     *clockSec = 0;
     int *clockNano = clockSec + 1;
@@ -82,6 +94,7 @@ int main(int argc, char* argv[]) {
     snprintf(argument, 10, "%d", m);
     int i;
 
+    // Fork and exec child processes
     for(i = 0; i <= n - s; i++) {
         wait();
         child = fork();
@@ -89,16 +102,19 @@ int main(int argc, char* argv[]) {
         if (child == 0) {
             execlp("./worker", "./worker", argument, (char *) NULL);
 
+	    // Exec failed if this prints
             fprintf(stderr, "%s failed to exec. Terminating program.", argv[0]);
             exit(-1);
         }
     }
 
+    // Wait for child processes to end and print the results from shared memory
     wait();
 
     printf("\nSeconds: %d\n", *clockSec);
     printf("Nanoseconds: %d\n", *clockNano);
 
+    // Deallocate shared memory
     if(shmdt(shmem) == -1) {
         perror("Error: shmdt:\n");
     }
@@ -111,6 +127,7 @@ int main(int argc, char* argv[]) {
 
 }
 
+// Deallocates shared memory and terminates if the timer goes off
 void alarmTime(int sig) {
     signal(sig, SIG_IGN);
     printf("Program has been running for 2 seconds. Ending all processes.\n");
@@ -125,6 +142,7 @@ void alarmTime(int sig) {
 
 }
 
+// Deallocates shared memory and terminates if 'crtl + c' is pressed
 void alarmInterrupt(int sig) {
     signal(sig, SIG_IGN);
     printf("\'ctrl + c\' pressed. Ending all processes.\n");
--- worker.c
+++ worker.c
@@ -12,14 +12,17 @@ int shmid;
 
 int main(int argc, char *argv[]) {
 
+    // Get value of m from passed argument
     int m = atoi(argv[1]);
 
+    // Access shared memory created by oss.c
     shmid = shmget(SHMKEY, BUFF_SIZE, 0777);
     if(shmid == -1) {
         perror("Error: shmget:\n");
         exit(1);
     }
 
+    // Access clock values from shared memory
     int *shmem = (int *)(shmat(shmid, 0, 0));
     int *clockSec = shmem;
     int *clockNano = clockSec + 1;
@@ -27,6 +30,8 @@ int main(int argc, char *argv[]) {
     int increase = m * 1000000;
     int i = 0;
 
+    // Loops m times, incrementing nanoseconds by m*1000000
+    // If nanoseconds > 1000000000, increment seconds accordingly
     while(i != m) {
 	*clockNano += increase;
 	if(*clockNano >= 1000000000) {
@@ -36,6 +41,7 @@ int main(int argc, char *argv[]) {
 	    }
 	}
 
+	// Print confirmation of worker changing clock values
         int pid = getpid();
 	i++;
         printf("worker_pid %d : ", pid);
